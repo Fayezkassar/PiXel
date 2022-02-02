d0b74d1961b0328a8dcec0e264dd95993b03407f
Paola <paolacharabaty@gmail.com>
2022-02-02 11:29:35 +0200

    [develop] Added the windows community toolkit for

--- ImageResizingApp/ViewModels/GetStartedViewModel.cs
+++ ImageResizingApp/ViewModels/GetStartedViewModel.cs
@@ -1,10 +1,20 @@
-﻿using System;
+﻿using CommunityToolkit.Mvvm.Input;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Windows.Input;
 
 namespace ImageResizingApp.ViewModels
 {
     public class GetStartedViewModel: ViewModelBase
     {
+        ICommand GetStartedCommand { get; }
+
+        public GetStartedViewModel()
+        {
+            GetStartedCommand = new RelayCommand(GetStarted);
+        }
+
+        private void GetStarted() => Console.WriteLine("Getting Started");
     }
 }
--- ImageResizingApp/ViewModels/ViewModelBase.cs
+++ ImageResizingApp/ViewModels/ViewModelBase.cs
@@ -1,4 +1,4 @@
-﻿using ImageResizingApp.Core;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
 using System;
 using System.Collections.Generic;
 using System.Text;
